e82e4a2fa5bfe75d407f2daf3e78fb302b50c636
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-11-02 16:59:28 +0900

    search: add tests

---
+++ search/test.cc
@@ -0,0 +1,36 @@
+#include <string>
+#include <unordered_map>
+
+#include "absl/container/flat_hash_map.h"
+#include "gtest/gtest.h"
+#include "kmer.h"
+
+TEST(kmer, String) {
+  const std::string s = "AGCTG";
+  Kmer<5> kmer(s);
+  ASSERT_EQ(kmer.String(), s);
+}
+
+TEST(kmer, Next) {
+  Kmer<5> kmer("AGCTG");
+  ASSERT_EQ(kmer.Next('C').String(), "GCTGC");
+}
+
+TEST(kmer, Prev) {
+  Kmer<5> kmer("AGCTG");
+  ASSERT_EQ(kmer.Prev('C').String(), "CAGCT");
+}
+
+TEST(kmer, UnorderedMapKey) {
+  Kmer<5> kmer("AGCTG");
+  std::unordered_map<Kmer<5>, int> m;
+  m[kmer] = 1;
+  ASSERT_EQ(m[kmer], 1);
+}
+
+TEST(kmer, FlatHashMapKey) {
+  Kmer<5> kmer("AGCTG");
+  absl::flat_hash_map<Kmer<5>, int> m;
+  m[kmer] = 1;
+  ASSERT_EQ(m[kmer], 1);
+}
