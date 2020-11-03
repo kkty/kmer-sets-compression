1a3085c3ee1d50ce67ce39705b74409fec4198e0
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-11-03 10:42:39 +0900

    add tests for KmerSet.Find()

--- test/kmer_set.cc
+++ test/kmer_set.cc
@@ -14,3 +14,25 @@ TEST(kmer_set, BucketAndKey) {
   const auto [bucket, key] = GetBucketAndKeyFromKmer<K, N>(kmer);
   ASSERT_EQ(kmer, (GetKmerFromBucketAndKey<K, N>(bucket, key)));
 }
+
+TEST(kmer_set, Find) {
+  const int K = 5;
+  const int B = 3;
+  KmerSet<K, B> kmer_set;
+  kmer_set.Add(Kmer<K>("AAAAA"));
+  kmer_set.Add(Kmer<K>("CCCCC"));
+
+  {
+    const auto kmers = kmer_set.Find(
+        [](const Kmer<K>& kmer) { return kmer.String()[0] == 'A'; });
+    ASSERT_EQ(kmers.size(), 1);
+    ASSERT_EQ(kmers[0].String(), "AAAAA");
+  }
+
+  {
+    const auto kmers = kmer_set.Find(
+        [](const Kmer<K>& kmer) { return kmer.String()[1] == 'C'; });
+    ASSERT_EQ(kmers.size(), 1);
+    ASSERT_EQ(kmers[0].String(), "CCCCC");
+  }
+}
