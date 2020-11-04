a2e6aa0c4a90083a0540f5d43c92ad20d7cabc59
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-11-04 17:44:28 +0900

    update tests for BidirectionalGraph.MST()

--- test/graph.cc
+++ test/graph.cc
@@ -23,14 +23,24 @@ TEST(UnionFind, UniteAndFind) {
 
 TEST(BidirectionalGraph, MST) {
   BidirectionalGraph g;
-  g.AddEdge(1, 2, 100);
-  g.AddEdge(2, 3, 100);
-  g.AddEdge(3, 4, 100);
-  g.AddEdge(1, 4, 1);
+
+  for (int i = 1; i <= 4; i++) {
+    for (int j = i + 1; j <= 5; j++) {
+      if ((i == 1 && j == 2) || (i == 1 && j == 3) || (i == 2 && j == 4) ||
+          (i == 2 && j == 5))
+        g.AddEdge(i, j, 100);
+      else
+        g.AddEdge(i, j, 1000);
+    }
+  }
+
   const auto [cost, tree] = g.MST(1);
-  ASSERT_EQ(cost, 201);
+
+  ASSERT_EQ(cost, 400);
   ASSERT_EQ(tree.Root(), 1);
+  ASSERT_EQ(tree.Size(), 5);
   ASSERT_EQ(tree.Parent(2), 1);
-  ASSERT_EQ(tree.Parent(3), 2);
-  ASSERT_EQ(tree.Parent(4), 1);
+  ASSERT_EQ(tree.Parent(3), 1);
+  ASSERT_EQ(tree.Parent(4), 2);
+  ASSERT_EQ(tree.Parent(5), 2);
 }
