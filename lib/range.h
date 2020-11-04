6c1adf2bca4459c55c8302ea758cdf84d602c75b
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-11-04 13:57:10 +0900

    update SplitRange()

--- lib/range.h
+++ lib/range.h
@@ -6,11 +6,27 @@
 
 std::vector<std::pair<int64_t, int64_t>> SplitRange(int64_t begin, int64_t end,
                                                     int64_t n) {
-  int chunk_size = (end - begin + n - 1) / n;
+  const int small_chunk_size = (end - begin) / n;
+  const int large_chunk_size = small_chunk_size + 1;
+
+  const int large_chunk_n = (end - begin) - small_chunk_size * n;
+  const int small_chunk_n = n - large_chunk_n;
 
   std::vector<std::pair<int64_t, int64_t>> ranges;
-  for (int64_t i = 0; i < n; i++) {
-    ranges.emplace_back(i * chunk_size, std::min((i + 1) * chunk_size, end));
+  ranges.reserve(n);
+
+  for (int64_t i = 0; i < small_chunk_n; i++) {
+    if (i == 0) {
+      ranges.emplace_back(begin, begin + small_chunk_size);
+    } else {
+      ranges.emplace_back(ranges.back().second,
+                          ranges.back().second + small_chunk_size);
+    }
+  }
+
+  for (int64_t i = 0; i < large_chunk_n; i++) {
+    ranges.emplace_back(ranges.back().second,
+                        ranges.back().second + large_chunk_size);
   }
 
   return ranges;
