788d65b298267fc3b4b6b2f7e911e7bc030bd22e
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-11-05 21:23:34 +0900

    update compress

--- src/compress.cc
+++ src/compress.cc
@@ -10,6 +10,7 @@
 #include "absl/flags/parse.h"
 #include "boost/iostreams/filter/bzip2.hpp"
 #include "boost/iostreams/filtering_stream.hpp"
+#include "graph.h"
 #include "kmer.h"
 #include "kmer_counter.h"
 #include "kmer_set.h"
@@ -24,11 +25,15 @@ ABSL_FLAG(int, cutoff, 1, "cut off threshold");
 int main(int argc, char** argv) {
   std::ios_base::sync_with_stdio(false);
 
+  // List of FASTQ file names.
   const std::vector<std::string> files = [&] {
     std::vector<std::string> v;
+
     for (const auto arg : absl::ParseCommandLine(argc, argv)) {
       v.push_back(arg);
     }
+
+    // Program name.
     v.erase(v.begin());
     return v;
   }();
@@ -71,14 +76,68 @@ int main(int argc, char** argv) {
     kmer_sets[i] = std::move(kmer_set);
   }
 
-  // Add an empty set in the back.
+  for (int i = 0; i < n_datasets; i++) {
+    spdlog::info("kmer_sets[{}].Size() = {}", i, kmer_sets[i].Size());
+  }
+
+  // kmer_sets[n_datasets] is an empty set.
   kmer_sets.push_back(KmerSet<K, B>{});
 
+  // Returns the required size to represent the difference between s1 and s2.
+  const auto get_diff = [&](const KmerSet<K, B>& s1, const KmerSet<K, B>& s2) {
+    // Returns the size after compaction.
+    const auto get_size = [&](KmerSet<K, B> s) {
+      const std::vector<std::string> unitigs = GetUnitigs(s);
+      int64_t size = 0;
+      for (const std::string& unitig : unitigs) size += unitig.size();
+      return size;
+    };
+
+    return get_size(s1 - s2) + get_size(s2 - s1);
+  };
+
   for (int i = 0; i < n_datasets; i++) {
-    for (int j = i + 1; j < n_datasets + 1; j++) {
-      int64_t diff = (KmerSetCompact{kmer_sets[i] - kmer_sets[j]}).Size() +
-                     (KmerSetCompact{kmer_sets[j] - kmer_sets[i]}).Size();
+    const int64_t diff = get_diff(kmer_sets[n_datasets], kmer_sets[i]);
+    spdlog::info("i = {}, diff = {}", i, diff);
+  }
+
+  {
+    for (int i = 0; i < n_datasets; i++) {
+      for (int j = i + 1; j < n_datasets; j++) {
+        spdlog::debug("calculating intersection: i = {}, j = {}", i, j);
+        KmerSet<K, B> intersection =
+            kmer_sets[i] - (kmer_sets[i] - kmer_sets[j]);
+        spdlog::debug("calculated intersection: i = {}, j = {}", i, j);
+        kmer_sets.push_back(std::move(intersection));
+      }
+    }
+  }
+
+  BidirectionalGraph g;
+  for (int i = 0; i < (int)kmer_sets.size(); i++) {
+    for (int j = i + 1; j < (int)kmer_sets.size(); j++) {
+      const int64_t diff = get_diff(kmer_sets[i], kmer_sets[j]);
       spdlog::info("i = {}, j = {}, diff = {}", i, j, diff);
+      g.AddEdge(i, j, get_diff(kmer_sets[i], kmer_sets[j]));
+    }
+  }
+
+  // Constructs the MST whose root is an empty set.
+  const auto [cost, tree] = g.MST(n_datasets);
+
+  spdlog::info("cost = {}", cost);
+
+  // BFS from root.
+  {
+    std::queue<int64_t> queue;
+    queue.push(tree.Root());
+    while (!queue.empty()) {
+      int64_t n = queue.front();
+      queue.pop();
+      for (int64_t child : tree.Children(n)) {
+        queue.push(child);
+        spdlog::info("tree.Parent({}) = {}", child, n);
+      }
     }
   }
 }
