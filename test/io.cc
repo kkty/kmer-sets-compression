5bcdda72811eb65507e379cefcb7276635bfe8d7
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-11-16 13:44:35 +0900

    add tests for ReadLines() and WriteLines()

---
+++ test/io.cc
@@ -0,0 +1,37 @@
+#include "io.h"
+
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+std::vector<std::string> GetTestData() {
+  std::vector<std::string> lines;
+
+  for (int i = 0; i < 1000000; i++) {
+    if (i % 2)
+      lines.push_back("foo");
+    else
+      lines.push_back("bar");
+  }
+
+  return lines;
+}
+
+TEST(io, WriteAndRead) {
+  const std::string file_name = "/tmp/data";
+  const std::vector<std::string> lines = GetTestData();
+
+  WriteLines(file_name, lines);
+
+  ASSERT_EQ(ReadLines(file_name), lines);
+}
+
+TEST(io, WriteAndReadCompressed) {
+  const std::string file_name = "/tmp/data";
+  const std::vector<std::string> lines = GetTestData();
+
+  WriteLines(file_name, "gzip", lines);
+
+  ASSERT_EQ(ReadLines(file_name, "gzcat"), lines);
+}
