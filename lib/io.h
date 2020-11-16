1138c0c8288e3e13549aa70cf6c9652b3f37231b
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-11-16 13:44:09 +0900

    add WriteLines()

--- lib/io.h
+++ lib/io.h
@@ -1,6 +1,7 @@
 #ifndef IO_H_
 #define IO_H_
 
+#include <fstream>
 #include <istream>
 #include <string>
 #include <vector>
@@ -12,7 +13,7 @@ std::vector<std::string> ReadLines(std::istream& is) {
 
   std::string s;
   while (std::getline(is, s)) {
-    lines.push_back(s);
+    lines.push_back(std::move(s));
   }
 
   return lines;
@@ -20,24 +21,52 @@ std::vector<std::string> ReadLines(std::istream& is) {
 
 // Opens a file and reads lines in it.
 std::vector<std::string> ReadLines(const std::string& file_name) {
-  std::ifstream is(file_name);
-
-  return ReadLines(is);
+  std::ifstream file(file_name);
+  return ReadLines(file);
 }
 
 // Opens a file, decompresses it, and reads lines in it.
 // Example: ReadLines("foo.gz", "gzcat")
 std::vector<std::string> ReadLines(const std::string& file_name,
                                    const std::string& decompressor) {
-  boost::process::ipstream ipstream;
-  boost::process::child child(decompressor + ' ' + file_name,
-                              boost::process::std_out > ipstream);
+  boost::process::ipstream out;
 
-  std::vector<std::string> lines = ReadLines(ipstream);
+  boost::process::child child(
+      decompressor,
+      boost::process::std_in<file_name, boost::process::std_out> out,
+      boost::process::std_err > boost::process::null);
+
+  std::vector<std::string> lines = ReadLines(out);
 
   child.wait();
 
   return lines;
+}
+
+void WriteLines(std::ostream& os, const std::vector<std::string>& lines) {
+  for (const auto& line : lines) os << line << '\n';
+}
+
+void WriteLines(const std::string& file_name,
+                const std::vector<std::string>& lines) {
+  std::ofstream file(file_name);
+  WriteLines(file, lines);
+}
+
+void WriteLines(const std::string& file_name, const std::string& compressor,
+                const std::vector<std::string>& lines) {
+  boost::process::opstream in;
+
+  boost::process::child child(
+      compressor, boost::process::std_in<in, boost::process::std_out> file_name,
+      boost::process::std_err > boost::process::null);
+
+  WriteLines(in, lines);
+
+  in.close();
+  in.pipe().close();
+
+  child.wait();
 }
 
 #endif
