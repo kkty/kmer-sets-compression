93d8811944a66843c32cb4e4892935c5f7888000
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-12-01 11:07:29 +0900

    decompress: take multiple files as arguments

--- src/decompress.cc
+++ src/decompress.cc
@@ -1,11 +1,12 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
 #include "absl/flags/flag.h"
-#include "absl/flags/parse.h"
 #include "core/kmer.h"
 #include "core/kmer_set.h"
 #include "core/kmer_set_compact.h"
+#include "flags.h"
 #include "log.h"
 #include "spdlog/spdlog.h"
 
@@ -16,49 +17,63 @@ ABSL_FLAG(int, workers, 1, "number of workers");
 ABSL_FLAG(bool, canonical, false, "use canonical k-mers");
 
 template <int K, typename KeyType>
-void Main(const std::string& file_name) {
+void Main(const std::vector<std::string>& files) {
   InitDefaultLogger();
 
   if (absl::GetFlag(FLAGS_debug)) EnableDebugLogs();
 
   const int n_workers = absl::GetFlag(FLAGS_workers);
+  const bool canonical = absl::GetFlag(FLAGS_canonical);
+  const std::string decompressor = absl::GetFlag(FLAGS_decompressor);
 
-  spdlog::info("constructing kmer_set_compact");
-  const KmerSetCompact<K, KeyType> kmer_set_compact =
-      KmerSetCompact<K, KeyType>::Load(file_name,
-                                       absl::GetFlag(FLAGS_decompressor));
-  spdlog::info("constructed kmer_set_compact");
+  for (size_t i = 0; i < files.size(); i++) {
+    const std::string& file_name = files[i];
+    spdlog::info("processing: i = {}, file_name = {}", i, file_name);
 
-  spdlog::info("constructing kmer_set");
-  const KmerSet<K, KeyType> kmer_set =
-      kmer_set_compact.ToKmerSet(absl::GetFlag(FLAGS_canonical), n_workers);
-  spdlog::info("constructed kmer_set");
+    spdlog::info("constructing kmer_set_compact");
+    const KmerSetCompact<K, KeyType> kmer_set_compact =
+        KmerSetCompact<K, KeyType>::Load(file_name, decompressor);
+    spdlog::info("constructed kmer_set_compact");
 
-  spdlog::info("kmer_set.Size() = {}", kmer_set.Size());
-  spdlog::info("kmer_set.Hash() = {}", kmer_set.Hash(n_workers));
+    spdlog::info("constructing kmer_set");
+    const KmerSet<K, KeyType> kmer_set =
+        kmer_set_compact.ToKmerSet(canonical, n_workers);
+    spdlog::info("constructed kmer_set");
+
+    int64_t size = kmer_set.Size();
+    size_t hash = kmer_set.Hash(n_workers);
+
+    spdlog::info("size = {}", size);
+    spdlog::info("hash = {}", hash);
+
+    std::cout << i << '\t' << file_name << '\t' << size << '\t' << hash
+              << std::endl;
+
+    spdlog::info("processed: i = {}, file_name = {}", i, file_name);
+  }
 }
 
 int main(int argc, char** argv) {
-  const std::string file_name = absl::ParseCommandLine(argc, argv)[1];
+  const std::vector<std::string> files = ParseFlags(argc, argv);
 
   const int k = absl::GetFlag(FLAGS_k);
 
   switch (k) {
     case 15:
       // 15 * 2 - 16 = 14 bits are used to select buckets.
-      Main<15, uint16_t>(file_name);
+      Main<15, uint16_t>(files);
       break;
     case 19:
       // 19 * 2 - 16 = 22 bits are used to select buckets.
-      Main<19, uint16_t>(file_name);
+      Main<19, uint16_t>(files);
       break;
     case 23:
       // 23 * 2 - 32 = 14 bits are used to select buckets.
-      Main<23, uint32_t>(file_name);
+      Main<23, uint32_t>(files);
       break;
     case 27:
       // 27 * 2 - 32 = 22 bits are used to select buckets.
-      Main<27, uint32_t>(file_name);
+      Main<27, uint32_t>(files);
       break;
     default:
       spdlog::error("unsupported k value");
