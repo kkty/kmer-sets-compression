49641cc1de03cac8a9b2ec3a3da8407e83935379
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-12-02 10:19:16 +0900

    print thread ids in logs

--- lib/log.h
+++ lib/log.h
@@ -1,11 +1,15 @@
 #ifndef LOG_H_
 #define LOG_H_
 
+#include <memory>
+
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
 
 void InitDefaultLogger() {
-  spdlog::set_default_logger(spdlog::stderr_color_mt("default"));
+  std::shared_ptr<spdlog::logger> logger = spdlog::stderr_color_mt("default");
+  logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] [%t] %v");
+  spdlog::set_default_logger(logger);
 }
 
 void EnableDebugLogs() { spdlog::set_level(spdlog::level::debug); }
