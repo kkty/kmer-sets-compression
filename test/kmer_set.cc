046ff4646730baf20a9079f014a75e9f90731b9c
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-12-04 10:50:42 +0900

    add tests for KmerSet

--- test/kmer_set.cc
+++ test/kmer_set.cc
@@ -2,16 +2,22 @@
 
 #include <bitset>
 #include <tuple>
+#include <vector>
 
-#include "gtest/gtest.h"
 #include "core/kmer.h"
 #include "core/kmer_counter.h"
+#include "gtest/gtest.h"
 
 TEST(KmerSet, BucketAndKey) {
   const int K = 5;
   using KeyType = uint8_t;
+
   Kmer<K> kmer("AGCTG");
-  const auto [bucket, key] = GetBucketAndKeyFromKmer<K, KeyType>(kmer);
+
+  int bucket;
+  KeyType key;
+  std::tie(bucket, key) = GetBucketAndKeyFromKmer<K, KeyType>(kmer);
+
   ASSERT_EQ(kmer.String(),
             (GetKmerFromBucketAndKey<K, KeyType>(bucket, key)).String());
 }
@@ -47,21 +53,21 @@ TEST(KmerSet, Find) {
   kmer_set.Add(Kmer<K>("CCCCC"));
 
   {
-    const auto kmers = kmer_set.Find(
+    const std::vector<Kmer<K>> kmers = kmer_set.Find(
         [](const Kmer<K>& kmer) { return kmer.String()[0] == 'A'; }, 1);
     ASSERT_EQ(kmers.size(), 1);
     ASSERT_EQ(kmers[0].String(), "AAAAA");
   }
 
   {
-    const auto kmers = kmer_set.Find(
+    const std::vector<Kmer<K>> kmers = kmer_set.Find(
         [](const Kmer<K>& kmer) { return kmer.String()[1] == 'C'; }, 1);
     ASSERT_EQ(kmers.size(), 1);
     ASSERT_EQ(kmers[0].String(), "CCCCC");
   }
 }
 
-TEST(KmetSet, Operators) {
+TEST(KmerSet, Operators) {
   const int K = 5;
   using KeyType = uint8_t;
 
@@ -84,7 +90,7 @@ TEST(KmetSet, Operators) {
   ASSERT_EQ(Intersection(kmer_set2, kmer_set1, 1).Size(), 2);
 }
 
-TEST(KmetSet, EqualsAndDiff) {
+TEST(KmerSet, EqualsAndDiff) {
   const int K = 5;
   using KeyType = uint8_t;
 
@@ -120,4 +126,22 @@ TEST(KmetSet, EqualsAndDiff) {
 
   ASSERT_DOUBLE_EQ(kmer_set1.Similarity(kmer_set3, 1), 0.25);
   ASSERT_DOUBLE_EQ(kmer_set3.Similarity(kmer_set1, 1), 0.25);
+}
+
+TEST(KmerSet, Extract) {
+  const int K = 5;
+  using KeyType = uint8_t;
+
+  KmerSet<K, KeyType> kmer_set;
+  kmer_set.Add(Kmer<K>("ACGTA"));
+  kmer_set.Add(Kmer<K>("CGTAC"));
+
+  const int K2 = 4;
+  using KeyType2 = uint8_t;
+
+  KmerSet<K2, KeyType2> extracted = kmer_set.template Extract<K2, KeyType2>(1);
+  ASSERT_EQ(extracted.Size(), 3);
+  ASSERT_TRUE(extracted.Contains(Kmer<K2>("ACGT")));
+  ASSERT_TRUE(extracted.Contains(Kmer<K2>("CGTA")));
+  ASSERT_TRUE(extracted.Contains(Kmer<K2>("GTAC")));
 }
