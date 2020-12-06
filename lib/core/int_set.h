f84f9d8c50a33867d61aed6b7829c0baa3ebb11e
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-12-06 19:01:14 +0900

    add IntSet and tests for IntSet

---
+++ lib/core/int_set.h
@@ -0,0 +1,362 @@
+#ifndef CORE_INT_SET_H_
+#define CORE_INT_SET_H_
+
+#include <cstdlib>
+#include <vector>
+
+#include "streamvbyte.h"
+
+// IntSet can be used to store an immutable set of integers.
+template <typename T>
+class IntSet {
+ public:
+  IntSet() = default;
+
+  IntSet(const IntSet& other)
+      : first_(other.first_),
+        n_(other.n_),
+        compressed_diff_(new uint8_t[other.compressed_diff_size_]),
+        compressed_diff_size_(other.compressed_diff_size_) {
+    std::memcpy(compressed_diff_, other.compressed_diff_,
+                compressed_diff_size_);
+  }
+
+  IntSet(IntSet&& other)
+      : first_(other.first_),
+        n_(other.n_),
+        compressed_diff_(other.compressed_diff_),
+        compressed_diff_size_(other.compressed_diff_size_) {
+    other.n_ = 0;
+    other.compressed_diff_ = nullptr;
+  }
+
+  IntSet& operator=(const IntSet& other) {
+    delete[] compressed_diff_;
+
+    first_ = other.first_;
+    n_ = other.n_;
+    compressed_diff_size_ = other.compressed_diff_size_;
+    compressed_diff_ = new uint8_t[compressed_diff_size_];
+
+    std::memcpy(compressed_diff_, other.compressed_diff_,
+                compressed_diff_size_);
+
+    return *this;
+  }
+
+  IntSet& operator=(IntSet&& other) {
+    delete[] compressed_diff_;
+
+    first_ = other.first_;
+    n_ = other.n_;
+    compressed_diff_ = other.compressed_diff_;
+    compressed_diff_size_ = other.compressed_diff_size_;
+
+    other.compressed_diff_ = nullptr;
+    other.n_ = 0;
+
+    return *this;
+  }
+
+  IntSet(const std::vector<T>& v) : n_(v.size()) {
+    if (n_ == 0) {
+      return;
+    }
+
+    first_ = v.front();
+
+    if (n_ == 1) {
+      return;
+    }
+
+    std::vector<uint32_t> diff(n_ - 1);
+
+    for (int64_t i = 0; i < n_ - 1; i++) {
+      diff[i] = v[i + 1] - v[i];
+    }
+
+    compressed_diff_ = new uint8_t[streamvbyte_max_compressedbytes(n_ - 1)];
+
+    compressed_diff_size_ =
+        streamvbyte_encode(diff.data(), n_ - 1, compressed_diff_);
+  }
+
+  ~IntSet() {
+    if (compressed_diff_ == nullptr) {
+      return;
+    }
+
+    delete[] compressed_diff_;
+  }
+
+  std::vector<T> Decode() const {
+    std::vector<T> v;
+    ForEach([&](T i) { v.push_back(i); });
+    return v;
+  }
+
+  int64_t Size() const { return n_; }
+
+  template <typename FuncType>
+  void ForEach(FuncType func) const {
+    if (n_ == 0) return;
+
+    T current = first_;
+    std::vector<uint32_t> diff = Diff();
+    auto it = diff.begin();
+
+    while (true) {
+      func(current);
+      if (it == diff.end()) return;
+      current += *it;
+      it++;
+    }
+  }
+
+  template <typename FuncType>
+  void Intersection(const IntSet& other, FuncType func) {
+    const IntSet& lhs = *this;
+    const IntSet& rhs = other;
+
+    if (lhs.n_ == 0 || rhs.n_ == 0) return;
+
+    std::vector<uint32_t> diff_lhs = lhs.Diff();
+    std::vector<uint32_t> diff_rhs = rhs.Diff();
+
+    auto it_lhs = diff_lhs.begin();
+    auto it_rhs = diff_rhs.begin();
+
+    T current_lhs = lhs.first_;
+    T current_rhs = rhs.first_;
+
+    while (true) {
+      if (current_lhs < current_rhs) {
+        if (it_lhs == diff_lhs.end()) return;
+
+        current_lhs += *it_lhs;
+        it_lhs++;
+      } else if (current_lhs > current_rhs) {
+        if (it_rhs == diff_rhs.end()) return;
+
+        current_rhs += *it_rhs;
+        it_rhs++;
+      } else {
+        func(current_lhs);
+
+        if (it_lhs == diff_lhs.end() || it_rhs == diff_rhs.end()) return;
+
+        current_lhs += *it_lhs;
+        it_lhs++;
+
+        current_rhs += *it_rhs;
+        it_rhs++;
+      }
+    }
+  }
+
+  int64_t IntersectionSize(const IntSet& other) {
+    int64_t size = 0;
+    Intersection(other, [&](T) { size++; });
+    return size;
+  }
+
+  IntSet Intersection(const IntSet& other) {
+    std::vector<T> v;
+    Intersection(other, [&](T i) { v.push_back(i); });
+    return IntSet(v);
+  }
+
+  template <typename FuncType>
+  void Add(const IntSet& other, FuncType func) {
+    const IntSet& lhs = *this;
+    const IntSet& rhs = other;
+
+    if (lhs.n_ == 0 && rhs.n_ == 0) return;
+
+    if (lhs.n_ == 0) {
+      rhs.ForEach(func);
+      return;
+    }
+
+    if (rhs.n_ == 0) {
+      lhs.ForEach(func);
+      return;
+    }
+
+    std::vector<uint32_t> diff_lhs = lhs.Diff();
+    std::vector<uint32_t> diff_rhs = rhs.Diff();
+
+    auto it_lhs = diff_lhs.begin();
+    auto it_rhs = diff_rhs.begin();
+
+    T current_lhs = lhs.first_;
+    T current_rhs = rhs.first_;
+
+    while (true) {
+      if (current_lhs < current_rhs) {
+        func(current_lhs);
+
+        if (it_lhs == diff_lhs.end()) {
+          while (true) {
+            func(current_rhs);
+
+            if (it_rhs == diff_rhs.end()) return;
+
+            current_rhs += *it_rhs;
+            it_rhs++;
+          }
+        }
+
+        current_lhs += *it_lhs;
+        it_lhs++;
+      } else if (current_lhs > current_rhs) {
+        func(current_rhs);
+
+        if (it_rhs == diff_rhs.end()) {
+          while (true) {
+            func(current_lhs);
+
+            if (it_lhs == diff_lhs.end()) return;
+
+            current_lhs += *it_lhs;
+            it_lhs++;
+          }
+        }
+
+        current_rhs += *it_rhs;
+        it_rhs++;
+      } else {
+        func(current_lhs);
+
+        if (it_lhs == diff_lhs.end() && it_rhs == diff_rhs.end()) return;
+
+        if (it_lhs == diff_lhs.end()) {
+          while (true) {
+            if (it_rhs == diff_rhs.end()) return;
+
+            current_rhs += *it_rhs;
+            it_rhs++;
+
+            func(current_rhs);
+          }
+        }
+
+        if (it_rhs == diff_rhs.end()) {
+          while (true) {
+            if (it_lhs == diff_lhs.end()) return;
+
+            current_lhs += *it_lhs;
+            it_lhs++;
+
+            func(current_lhs);
+          }
+        }
+
+        current_lhs += *it_lhs;
+        it_lhs++;
+
+        current_rhs += *it_rhs;
+        it_rhs++;
+      }
+    }
+  }
+
+  int64_t AddSize(const IntSet& other) {
+    int64_t size = 0;
+    Add(other, [&](T) { size++; });
+    return size;
+  }
+
+  IntSet Add(const IntSet& other) {
+    std::vector<T> v;
+    Add(other, [&](T i) { v.push_back(i); });
+    return IntSet(v);
+  }
+
+  template <typename FuncType>
+  void Sub(const IntSet& other, FuncType func) {
+    const IntSet& lhs = *this;
+    const IntSet& rhs = other;
+
+    if (lhs.n_ == 0) return;
+
+    if (rhs.n_ == 0) {
+      lhs.ForEach(func);
+      return;
+    }
+
+    std::vector<uint32_t> diff_lhs = lhs.Diff();
+    std::vector<uint32_t> diff_rhs = rhs.Diff();
+
+    auto it_lhs = diff_lhs.begin();
+    auto it_rhs = diff_rhs.begin();
+
+    T current_lhs = lhs.first_;
+    T current_rhs = rhs.first_;
+
+    while (true) {
+      if (current_lhs < current_rhs) {
+        func(current_lhs);
+
+        if (it_lhs == diff_lhs.end()) return;
+
+        current_lhs += *it_lhs;
+        it_lhs++;
+      } else if (current_lhs > current_rhs) {
+        if (it_rhs == diff_rhs.end()) {
+          while (true) {
+            func(current_lhs);
+
+            if (it_lhs == diff_lhs.end()) return;
+
+            current_lhs += *it_lhs;
+            it_lhs++;
+          }
+        }
+
+        current_rhs += *it_rhs;
+        it_rhs++;
+      } else {
+        if (it_lhs == diff_lhs.end()) return;
+
+        if (it_rhs == diff_rhs.end()) {
+          while (true) {
+            if (it_lhs == diff_lhs.end()) return;
+
+            current_lhs += *it_lhs;
+            it_lhs++;
+
+            func(current_lhs);
+          }
+        }
+
+        current_lhs += *it_lhs;
+        it_lhs++;
+
+        current_rhs += *it_rhs;
+        it_rhs++;
+      }
+    }
+  }
+
+  IntSet Sub(const IntSet& other) {
+    std::vector<T> v;
+    Sub(other, [&](T i) { v.push_back(i); });
+    return IntSet(v);
+  }
+
+ private:
+  T first_;
+  int64_t n_ = 0;
+
+  std::vector<uint32_t> Diff() const {
+    std::vector<uint32_t> diff(n_ - 1);
+    streamvbyte_decode(compressed_diff_, diff.data(), n_ - 1);
+    return diff;
+  }
+
+  uint8_t* compressed_diff_ = nullptr;
+  int64_t compressed_diff_size_ = 0;
+};
+
+#endif
---
+++ test/int_set.cc
@@ -0,0 +1,153 @@
+#include "core/int_set.h"
+
+#include <algorithm>
+#include <limits>
+#include <vector>
+
+#include "absl/random/random.h"
+#include "gtest/gtest.h"
+
+TEST(IntSet, MoveConstructor) {
+  std::vector<int> v{1, 3, 5, 7, 9};
+  IntSet<int> int_set(v);
+  IntSet<int> moved = std::move(int_set);
+  ASSERT_EQ(moved.Decode(), v);
+  ASSERT_EQ(int_set.Size(), 0);  // NOLINT
+}
+
+TEST(IntSet, MoveAssignment) {
+  std::vector<int> v{1, 3, 5, 7, 9};
+  IntSet<int> int_set(v);
+  IntSet<int> moved;
+  moved = std::move(int_set);
+  ASSERT_EQ(moved.Decode(), v);
+  ASSERT_EQ(int_set.Size(), 0);  // NOLINT
+}
+
+TEST(IntSet, CopyConstructor) {
+  std::vector<int> v{1, 3, 5, 7, 9};
+  IntSet<int> int_set(v);
+  IntSet<int> copied(int_set);  // NOLINT
+  ASSERT_EQ(copied.Decode(), v);
+}
+
+TEST(IntSet, CopyAssignment) {
+  std::vector<int> v{1, 3, 5, 7, 9};
+  IntSet<int> int_set(v);
+  IntSet<int> copied;
+  copied = int_set;
+  ASSERT_EQ(copied.Decode(), v);
+}
+
+TEST(IntSet, EncodeAndDecodeRandom) {
+  absl::InsecureBitGen bitgen;
+
+  const int n = 1000000;
+  std::vector<int> v;
+  v.reserve(n);
+
+  for (int i = 0; i < n; i++) {
+    v.push_back(absl::Uniform<uint32_t>(absl::IntervalClosedClosed, bitgen,
+                                        std::numeric_limits<int>::min(),
+                                        std::numeric_limits<int>::max()));
+  }
+
+  std::sort(v.begin(), v.end());
+
+  {
+    auto it = std::unique(v.begin(), v.end());
+    v.resize(std::distance(v.begin(), it));
+  }
+
+  IntSet int_set(v);
+
+  ASSERT_EQ(int_set.Size(), v.size());
+
+  std::vector<int> decoded = int_set.Decode();
+
+  ASSERT_EQ(v, decoded);
+}
+
+TEST(IntSet, IntersectionAddSub) {
+  std::vector<int> v1{1, 3, 5, 7, 9};
+  std::vector<int> v2{0, 1, 5, 6, 9, 10};
+
+  {
+    std::vector<int> expected{1, 5, 9};
+    ASSERT_EQ(IntSet(v1).Intersection(IntSet(v2)).Decode(), expected);
+    ASSERT_EQ(IntSet(v1).Intersection(IntSet(v2)).Decode(), expected);
+  }
+
+  {
+    std::vector<int> expected{0, 1, 3, 5, 6, 7, 9, 10};
+    ASSERT_EQ(IntSet(v1).Add(IntSet(v2)).Decode(), expected);
+  }
+
+  {
+    std::vector<int> expected{3, 7};
+    ASSERT_EQ(IntSet(v1).Add(IntSet(v2)).Decode(), expected);
+  }
+}
+
+TEST(IntSet, IntersectionAddSubRandom) {
+  absl::InsecureBitGen bitgen;
+
+  const auto GetData = [&] {
+    std::vector<int> v;
+
+    const int n = absl::Uniform(bitgen, 0, 10000);
+    const int m = absl::Uniform(bitgen, 0, 10000);
+
+    v.reserve(n);
+
+    for (int i = 0; i < n; i++) {
+      v.push_back(absl::Uniform(absl::IntervalClosedClosed, bitgen, 0, m));
+    }
+
+    std::sort(v.begin(), v.end());
+
+    // Removes duplicates.
+    auto it = std::unique(v.begin(), v.end());
+    v.resize(std::distance(v.begin(), it));
+
+    return v;
+  };
+
+  std::vector<int> v1 = GetData();
+  std::vector<int> v2 = GetData();
+
+  {
+    std::vector<int> expected;
+    expected.insert(expected.end(), v1.begin(), v1.end());
+    expected.insert(expected.end(), v2.begin(), v2.end());
+    std::sort(expected.begin(), expected.end());
+    auto it = std::unique(expected.begin(), expected.end());
+    expected.resize(std::distance(expected.begin(), it));
+
+    ASSERT_EQ(IntSet(v1).Add(IntSet(v2)).Decode(), expected);
+  }
+
+  {
+    std::vector<int> expected;
+
+    for (int i : v1) {
+      if (std::find(v2.begin(), v2.end(), i) != v2.end()) {
+        expected.push_back(i);
+      }
+    }
+
+    ASSERT_EQ(IntSet(v1).Intersection(IntSet(v2)).Decode(), expected);
+  }
+
+  {
+    std::vector<int> expected;
+
+    for (int i : v1) {
+      if (std::find(v2.begin(), v2.end(), i) == v2.end()) {
+        expected.push_back(i);
+      }
+    }
+
+    ASSERT_EQ(IntSet(v1).Sub(IntSet(v2)).Decode(), expected);
+  }
+}
