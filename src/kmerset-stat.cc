49b5e95e9dcc1480feeb7590b7988ae7cf611e81
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-12-08 19:48:02 +0900

    add src/kmerset-stat.cc

---
+++ src/kmerset-stat.cc
@@ -0,0 +1,168 @@
+#include <algorithm>
+#include <cstdlib>
+#include <string>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/status/statusor.h"
+#include "core/kmer_set.h"
+#include "core/kmer_set_compact.h"
+#include "core/kmer_set_immutable.h"
+#include "flags.h"
+#include "io.h"
+#include "log.h"
+#include "spdlog/spdlog.h"
+
+ABSL_FLAG(int, k, 15, "the length of kmers");
+ABSL_FLAG(bool, debug, false, "enable debugging messages");
+ABSL_FLAG(std::string, decompressor, "",
+          "specify decompressor for FASTQ files");
+ABSL_FLAG(bool, canonical, false, "count canonical k-mers");
+ABSL_FLAG(int, workers, 1, "number of workers");
+
+template <int K, int N, typename KeyType>
+void Main(const std::vector<std::string>& files) {
+  InitDefaultLogger();
+
+  const bool debug = absl::GetFlag(FLAGS_debug);
+  const std::string decompressor = absl::GetFlag(FLAGS_decompressor);
+  const bool canonical = absl::GetFlag(FLAGS_canonical);
+  const int n_workers = absl::GetFlag(FLAGS_workers);
+
+  if (debug) EnableDebugLogs();
+
+  const int n_files = files.size();
+
+  std::vector<KmerSetImmutable<K, N, KeyType>> kmer_sets_immutable(n_files);
+
+  for (int i = 0; i < n_files; i++) {
+    const std::string& file = files[i];
+
+    spdlog::info("reading file: {}", file);
+
+    absl::StatusOr<KmerSet<K, N, KeyType>> statusor =
+        GetKmerSetFromCompressedKmersFile<K, N, KeyType>(file, decompressor,
+                                                         canonical, n_workers);
+
+    if (!statusor.ok()) {
+      spdlog::error("failed to read file: {}", statusor.status().ToString());
+      std::exit(1);
+    }
+
+    spdlog::info("finished reading file: {}", file);
+
+    const KmerSet<K, N, KeyType> kmer_set = std::move(statusor).value();
+
+    spdlog::info("kmer_set.Size() = {}", kmer_set.Size());
+    spdlog::info("kmer_set.Hash() = {}", kmer_set.Hash(n_workers));
+
+    KmerSetImmutable<K, N, KeyType> kmer_set_immutable(kmer_set, n_workers);
+
+    spdlog::info("kmer_set_immutable.Size() = {}", kmer_set_immutable.Size());
+    spdlog::info("kmer_set_immutable.Bytes() = {}", kmer_set_immutable.Bytes());
+
+    kmer_sets_immutable[i] = std::move(kmer_set_immutable);
+  }
+
+  {
+    int64_t total_size = 0;
+
+    for (int i = 0; i < n_files; i++) {
+      total_size += kmer_sets_immutable[i].Size();
+    }
+
+    spdlog::info("total_size = {}", total_size);
+  }
+
+  // Lists up all the k-mers that appear at least once.
+  {
+    KmerSetImmutable<K, N, KeyType> all;
+
+    for (int i = 0; i < n_files; i++) {
+      all = all.Add(kmer_sets_immutable[i], n_workers);
+    }
+
+    spdlog::info("all.Size() = {}", all.Size());
+    spdlog::info("all.Bytes() = {}", all.Bytes());
+  }
+
+  const auto GetSizeAfterCompaction =
+      [canonical,
+       n_workers](const KmerSetImmutable<K, N, KeyType>& kmer_set_immutable) {
+        return KmerSetCompact<K, N, KeyType>::FromKmerSet(
+                   kmer_set_immutable.ToKmerSet(n_workers), canonical,
+                   n_workers)
+            .Size(n_workers);
+      };
+
+  std::vector<int64_t> sizes_after_compaction(n_files);
+
+  for (int i = 0; i < n_files; i++) {
+    spdlog::info("i = {}", i);
+    const KmerSetImmutable<K, N, KeyType>& kmer_set_immutable =
+        kmer_sets_immutable[i];
+    spdlog::info("kmer_set_immutable.Size() = {}", kmer_set_immutable.Size());
+    int64_t size_after_compaction =
+        GetSizeAfterCompaction(kmer_sets_immutable[i]);
+    spdlog::info("size_after_compaction = {}", size_after_compaction);
+    sizes_after_compaction[i] = size_after_compaction;
+  }
+
+  for (int i = 0; i < n_files; i++) {
+    for (int j = i + 1; j < n_files; j++) {
+      spdlog::info("i = {}, j = {}", i, j);
+
+      spdlog::info("constructing intersection");
+      const KmerSetImmutable<K, N, KeyType> intersection =
+          kmer_sets_immutable[i].Intersection(kmer_sets_immutable[j],
+                                              n_workers);
+      spdlog::info("constructed intersection");
+
+      spdlog::info("constructing sub_i");
+      const KmerSetImmutable<K, N, KeyType> sub_i =
+          kmer_sets_immutable[i].Sub(intersection, n_workers);
+      spdlog::info("constructed sub_i");
+
+      spdlog::info("constructing sub_j");
+      const KmerSetImmutable<K, N, KeyType> sub_j =
+          kmer_sets_immutable[j].Sub(intersection, n_workers);
+      spdlog::info("constructed sub_j");
+
+      spdlog::info("intersection.Size() = {}", intersection.Size());
+      spdlog::info("sub_i.Size() = {}", sub_i.Size());
+      spdlog::info("sub_j.Size() = {}", sub_j.Size());
+
+      const int64_t original_size =
+          sizes_after_compaction[i] + sizes_after_compaction[j];
+
+      spdlog::info("original_size = {}", original_size);
+
+      const int64_t diff_size = GetSizeAfterCompaction(intersection) +
+                                GetSizeAfterCompaction(sub_i) +
+                                GetSizeAfterCompaction(sub_j) - original_size;
+
+      spdlog::info("diff_size = {}", diff_size);
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  const std::vector<std::string> files = ParseFlags(argc, argv);
+
+  const int k = absl::GetFlag(FLAGS_k);
+
+  switch (k) {
+    case 15:
+      Main<15, 14, uint16_t>(files);
+      break;
+    case 19:
+      Main<19, 10, uint32_t>(files);
+      break;
+    case 23:
+      Main<23, 14, uint32_t>(files);
+      break;
+    default:
+      spdlog::error("unsupported k value");
+      std::exit(1);
+  }
+}
