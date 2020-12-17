6246acf1807134cd8d32ba7d86a4bd09d899122d
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-12-17 15:04:00 +0900

    update tests for KmerSetCompact

--- test/kmer_set_compact.cc
+++ test/kmer_set_compact.cc
@@ -2,33 +2,61 @@
 
 #include <string>
 
+#include "absl/random/random.h"
 #include "core/kmer.h"
 #include "core/kmer_set.h"
 #include "gtest/gtest.h"
 
-TEST(KmerSetCompact, FromAndToKmerSet) {
-  const int K = 9;
-  const int N = 10;
-  using KeyType = uint8_t;
+template <int K, int N, typename KeyType>
+KmerSet<K, N, KeyType> GetTestData() {
+  absl::InsecureBitGen bitgen;
 
   KmerSet<K, N, KeyType> kmer_set;
 
-  // Adding random kmers.
-  for (int i = 0; i < 10000; i++) {
-    kmer_set.Add(GetRandomKmer<K>().Canonical());
-  }
+  for (int i = 0; i < absl::Uniform(bitgen, 0, 10000); i++) {
+    std::string s;
 
-  // Adding loops.
-  for (int i = 0; i < 1000; i++) {
-    const Kmer<K> kmer = GetRandomKmer<K>();
-    const std::string s = kmer.String() + kmer.String();
-    for (int j = 0; j < K; j++) {
+    for (int j = 0; j < absl::Uniform(bitgen, 0, 100); j++) {
+      s += GetRandomKmer<K>().String();
+    }
+
+    // Creates a loop.
+    if (absl::Uniform(bitgen, 0, 2) == 0) {
+      s += s;
+    }
+
+    for (int j = 0; j < static_cast<int>(s.size()) - K + 1; j++) {
       kmer_set.Add(Kmer<K>(s.substr(j, K)).Canonical());
     }
   }
 
+  return kmer_set;
+}
+
+TEST(KmerSetCompact, FromAndToKmerSetRandom) {
+  const int K = 9;
+  const int N = 10;
+  using KeyType = uint8_t;
+
+  KmerSet<K, N, KeyType> kmer_set = GetTestData<K, N, KeyType>();
+
   KmerSetCompact<K, N, KeyType> compressed =
       KmerSetCompact<K, N, KeyType>::FromKmerSet(kmer_set, true, false, 1);
+
+  KmerSet<K, N, KeyType> decompressed = compressed.ToKmerSet(true, 1);
+
+  ASSERT_TRUE(kmer_set.Equals(decompressed, 1));
+}
+
+TEST(KmerSetCompact, FromAndToKmerSetFastRandom) {
+  const int K = 9;
+  const int N = 10;
+  using KeyType = uint8_t;
+
+  KmerSet<K, N, KeyType> kmer_set = GetTestData<K, N, KeyType>();
+
+  KmerSetCompact<K, N, KeyType> compressed =
+      KmerSetCompact<K, N, KeyType>::FromKmerSet(kmer_set, true, true, 1);
 
   KmerSet<K, N, KeyType> decompressed = compressed.ToKmerSet(true, 1);
 
