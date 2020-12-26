f5eb56ae926f1d3b48f68de77c3513365f72374f
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-12-26 14:21:16 +0900

    add tests for KmerCounter::FromReads()

--- test/kmer_counter.cc
+++ test/kmer_counter.cc
@@ -1,23 +1,25 @@
 #include "core/kmer_counter.h"
 
+#include <cstdint>
 #include <limits>
 #include <tuple>
+#include <utility>
 
 #include "core/kmer.h"
 #include "core/kmer_set.h"
 #include "gtest/gtest.h"
 
 TEST(KmerCounter, AddWithMax) {
-  uint8_t x = 1;
-  x = AddWithMax(std::numeric_limits<uint8_t>::max(), x);
-  ASSERT_EQ(x, std::numeric_limits<uint8_t>::max());
+  std::uint8_t x = 1;
+  x = AddWithMax(std::numeric_limits<std::uint8_t>::max(), x);
+  ASSERT_EQ(x, std::numeric_limits<std::uint8_t>::max());
 }
 
 TEST(KmerCounter, AddAndGet) {
   const int K = 5;
   const int N = 3;
-  using KeyType = uint8_t;
-  using ValueType = uint8_t;
+  using KeyType = std::uint8_t;
+  using ValueType = std::uint8_t;
 
   KmerCounter<K, N, KeyType, ValueType> kmer_counter;
 
@@ -39,8 +41,8 @@ TEST(KmerCounter, ToAndFromKmerSet) {
   const int K = 5;
   const int N = 3;
   const int n_workers = 1;
-  using KeyType = uint8_t;
-  using ValueType = uint8_t;
+  using KeyType = std::uint8_t;
+  using ValueType = std::uint8_t;
 
   KmerCounter<K, N, KeyType, ValueType> kmer_counter;
 
@@ -64,4 +66,33 @@ TEST(KmerCounter, ToAndFromKmerSet) {
   ASSERT_EQ(reconstructed_kmer_counter.Size(), 2);
   ASSERT_EQ(reconstructed_kmer_counter.Get(Kmer<K>("AAAAA")), n_workers);
   ASSERT_EQ(reconstructed_kmer_counter.Get(Kmer<K>("TTTTT")), n_workers);
+}
+
+TEST(KmerCounter, FromReads) {
+  const int K = 5;
+  const int N = 3;
+  using KeyType = std::uint8_t;
+  using ValueType = std::uint8_t;
+  const int n_workers = 1;
+
+  const std::vector<std::string> reads{
+      "AACCGTT",
+      "AACCGTA",
+  };
+
+  KmerCounter<K, N, KeyType, ValueType> kmer_counter;
+
+  {
+    absl::StatusOr<KmerCounter<K, N, KeyType, ValueType>> statusor =
+        KmerCounter<K, N, KeyType, ValueType>::FromReads(reads, false,
+                                                         n_workers);
+
+    ASSERT_TRUE(statusor.ok());
+    kmer_counter = std::move(statusor).value();
+  }
+
+  ASSERT_EQ(kmer_counter.Get(Kmer<K>("AACCG")), 2);
+  ASSERT_EQ(kmer_counter.Get(Kmer<K>("ACCGT")), 2);
+  ASSERT_EQ(kmer_counter.Get(Kmer<K>("CCGTT")), 1);
+  ASSERT_EQ(kmer_counter.Get(Kmer<K>("CCGTA")), 1);
 }
