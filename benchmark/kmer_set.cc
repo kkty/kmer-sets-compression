b2ebc29250d4f92a387b086407654f437e3cafcd
Kazushi Kitaya <kazushikitaya@gmail.com>
2021-01-04 11:43:59 +0900

    benchmark: change n_workers in benchmarks for KmerSet

--- benchmark/kmer_set.cc
+++ benchmark/kmer_set.cc
@@ -8,16 +8,15 @@ void Benchmark_KmerSet_Find(benchmark::State& state) {
   const int K = 11;
   const int N = 14;
   using KeyType = std::uint8_t;
-  const int n_workers = 8;
 
   KmerSet<K, N, KeyType> kmer_set =
       GetRandomKmerSet<K, N, KeyType>(1'000'000, true);
 
   for (auto _ : state) {
-    benchmark::DoNotOptimize(kmer_set.Find(n_workers));
+    benchmark::DoNotOptimize(kmer_set.Find(state.range(0)));
   }
 }
 
-BENCHMARK(Benchmark_KmerSet_Find);
+BENCHMARK(Benchmark_KmerSet_Find)->RangeMultiplier(2)->Range(1, 8);
 
 BENCHMARK_MAIN();
