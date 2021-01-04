02f7a8e053c7b466c387ef8930b23ee721dbb2fe
Kazushi Kitaya <kazushikitaya@gmail.com>
2021-01-04 20:09:04 +0900

    add benchmarks for GetUnitigsCanonical() and GetSPSSCanonical()

--- benchmark/spss.cc
+++ benchmark/spss.cc
@@ -9,6 +9,36 @@
 #include "core/kmer_set.h"
 #include "random.h"
 
+void Benchmark_GetUnitigsCanonical(benchmark::State& state) {
+  const int K = 11;
+  const int N = 14;
+  using KeyType = std::uint8_t;
+
+  KmerSet<K, N, KeyType> kmer_set =
+      GetRandomKmerSet<K, N, KeyType>(1'000'000, true);
+
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(GetUnitigsCanonical(kmer_set, state.range(0)));
+  }
+}
+
+BENCHMARK(Benchmark_GetUnitigsCanonical)->RangeMultiplier(2)->Range(1, 8);
+
+void Benchmark_GetSPSSCanonical(benchmark::State& state) {
+  const int K = 11;
+  const int N = 14;
+  using KeyType = std::uint8_t;
+
+  KmerSet<K, N, KeyType> kmer_set =
+      GetRandomKmerSet<K, N, KeyType>(1'000'000, true);
+
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(GetSPSSCanonical(kmer_set, true, state.range(0)));
+  }
+}
+
+BENCHMARK(Benchmark_GetSPSSCanonical)->RangeMultiplier(2)->Range(1, 8);
+
 void Benchmark_GetSPSSWeightCanonical(benchmark::State& state) {
   const int K = 11;
   const int N = 14;
