0e52cee7c10b37bd6a175e971ecc3ee424e8cb27
Kazushi Kitaya <kazushikitaya@gmail.com>
2021-01-08 22:54:35 +0900

    remove benchmarks for GetKmersFromSPSS()

--- benchmark/spss.cc
+++ benchmark/spss.cc
@@ -2,8 +2,6 @@
 
 #include <cstdint>
 #include <string>
-#include <thread>
-#include <vector>
 
 #include "benchmark/benchmark.h"
 #include "core/kmer_set.h"
@@ -53,23 +51,5 @@ void Benchmark_GetSPSSWeightCanonical(benchmark::State& state) {
 }
 
 BENCHMARK(Benchmark_GetSPSSWeightCanonical)->RangeMultiplier(2)->Range(1, 8);
-
-void Benchmark_GetKmersFromSPSS(benchmark::State& state) {
-  const int K = 11;
-  const int N = 14;
-  using KeyType = std::uint8_t;
-
-  KmerSet<K, N, KeyType> kmer_set =
-      GetRandomKmerSet<K, N, KeyType>(1'000'000, true);
-
-  std::vector<std::string> spss =
-      GetSPSSCanonical(kmer_set, true, std::thread::hardware_concurrency());
-
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(GetKmersFromSPSS<K>(spss, true, state.range(0)));
-  }
-}
-
-BENCHMARK(Benchmark_GetKmersFromSPSS)->RangeMultiplier(2)->Range(1, 8);
 
 BENCHMARK_MAIN();
